8d750cd92ee07d3741c8d110ff95293b3eb6c23a
Dell <Dell@DESKTOP-9VK4AQ1>
2017-09-17 00:15:56 +0530

    Forth commit and deleted second stmt

--- Demo/src/demo/staging/demo.java
+++ Demo/src/demo/staging/demo.java
@@ -4,7 +4,6 @@ public class demo {
 
 	public static void main(String[] args) {
 		System.out.println("first commit");
-		System.out.println("second commit");
 		System.out.println("Third Commit");
 
 	}
