d3327d8dcdfa81fc21b637009dbef9b1ee8c54a4
Dell <Dell@DESKTOP-9VK4AQ1>
2017-09-17 00:14:12 +0530

    Third Commit

--- Demo/src/demo/staging/demo.java
+++ Demo/src/demo/staging/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("first commit");
 		System.out.println("second commit");
+		System.out.println("Third Commit");
 
 	}
 
