f2367005a80b567995e7b23ed18237aab120ff5a
marrisrinu <marrisrinu1987@gmail.com>
2017-09-17 12:53:34 +0530

    new commit

--- Demo/src/demo/staging/demo.java
+++ Demo/src/demo/staging/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("first commit");
 		System.out.println("Third Commit");
+		System.out.println("Forth Commit");
 
 	}
 
